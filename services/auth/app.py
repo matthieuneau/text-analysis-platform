3493c6188dba21af9e271e553cbb301afa13d702
Matthieu Neau <matthieu.neau@datadoghq.com>
2025-06-10 22:21:10 -0400

    containerize auth service

--- services/auth/app.py
+++ services/auth/app.py
@@ -93,6 +93,6 @@ if __name__ == "__main__":
         app,
         host="0.0.0.0",
         port=8000,
-        reload=True,  # Remove in production
+        # reload=True,  # Remove in production
         log_level="info",
     )
